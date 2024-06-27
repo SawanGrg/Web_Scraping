c10442f51075f5580dcec406ed812ec50eab98ce
SawanGurung <you@example.com>
2024-06-27 10:03:45 +0545

    update

--- webScraping.py
+++ webScraping.py
@@ -5,47 +5,53 @@ from bs4 import BeautifulSoup
 root_url = 'https://quotes.toscrape.com/page/'
 page_initial_size = 1
 
-#pachi we can break down this code into SOLID Acronym ko first principle: Single Responsibility Principle
+file_path = 'D:\holder.txt'
+
 def extract_quotes(divs):
-        quotes = []
-        for tag in divs:
-            quote = tag.find('span', attrs = {'class':'text'})
-            quotes.append(quote.text)
+    quotes = []
+    for tag in divs:
+        quote = tag.find('span', attrs={'class': 'text'})
+        quotes.append(quote.text.strip())
+    return quotes
 
-            f = open("D:\holder.txt", "a+", encoding="utf-8")
-            f.write(quote.text + "\n")
-        return quotes
-
-#pachi we break down this code into SOLID Acronym ko first principle: single responsibility principle
-def extract_author(divs):
-        authors = []
-        for tag in divs:
-            author = tag.find('small', attrs = {'class':'author'})
-            authors.append(author.text)
-            f = open("D:\holder.txt", "a+", encoding="utf-8")
-            f.write(author.text + "\n")
-        return authors
+def extract_authors(divs):
+    authors = []
+    for tag in divs:
+        author = tag.find('small', attrs={'class': 'author'})
+        authors.append(author.text.strip())
+    return authors
 
 
-#time complexity: O(n) huncha hai dai esko 
-#for each iteration, we are making a request to the server and parsing the response with new pagination
-for i in range(1, 4):
-    response = req.get(root_url + str(i))
+def extract_tags(divs):
+    tags = []
+    for div in divs:
+        tag_elements = div.find_all('div', class_='tags')
+        for tag in tag_elements:
+            each_tag = tag.find('a', class_='tag')
+            tags.append(each_tag.text)
+    return tags
 
-    if response.status_code == 200:
-        print("Request was successful")
-    else:
-        print("Request failed")
+with open(file_path, "a+", encoding="utf-8") as f:
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+        #time complexity: O(n) huncha hai dai esko 
+        #for each iteration, we are making a request to the server and parsing the response with new pagination
+        for i in range(1, 4):
+            response = req.get(root_url + str(i))
 
-    quote_divs = soup.find_all('div', attrs={'class': 'quote'})
+            if response.status_code == 200:
+                print("Request was successful")
+            else:
+                print("Request failed")
 
-    quotes = extract_quotes(quote_divs)
-    authors = extract_author(quote_divs)
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    for quote in quotes:
-        print(quote)
+            quote_divs = soup.find_all('div', attrs={'class': 'quote'})
 
-    for author in authors:
-        print(author)
+            quotes = extract_quotes(quote_divs)
+            authors = extract_authors(quote_divs)
+            tags = extract_tags(quote_divs)
+
+            for q, a, t in zip(quotes, authors, tags):
+                    formatted_quote = f'"{q}" - {a} [{t}]'
+                    print(formatted_quote)
+                    f.write(formatted_quote + "\n")
